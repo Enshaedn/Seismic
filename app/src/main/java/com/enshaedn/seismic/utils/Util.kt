bd2ad246641d3d9892434c5880d55410ac059384
Enshaedn <alexclontz@gmail.com>
2021-03-29 12:40:17 -0700

    adding recyclerview to sessions list

---
+++ app/src/main/java/com/enshaedn/seismic/utils/Util.kt
@@ -0,0 +1,6 @@
+package com.enshaedn.seismic.utils
+
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class SessionItemViewHolder(val textView: TextView) : RecyclerView.ViewHolder(textView)
